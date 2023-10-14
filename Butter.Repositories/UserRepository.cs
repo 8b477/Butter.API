3cf1f06baacc1e483456aae03eee29ea8820c6e8
8b477 <jonathan_buchet@outlook.fr>
2023-10-14 13:28:38 +0200

    Ajoutez des fichiers projet.

---
+++ Butter.Repositories/UserRepository.cs
@@ -0,0 +1,62 @@
+﻿using Butter.Entities;
+using Butter.Models;
+using Butter.Repositories.Interfaces;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Butter.Repositories
+{
+    public class UserRepository : BaseRepository<UserModel, UserEntity, int>, IUserRepository
+    {
+        public UserRepository(string cnstr) : base(cnstr)
+        {
+        }
+
+        /// <summary>
+        /// Récupère un modèle et le transforme en Entité pour la base de donnée
+        /// </summary>
+        /// <param name="Model"></param>
+        /// <returns></returns>
+        public override UserEntity? ToEntite(UserModel Model)
+        {
+            if (Model is null) return null;
+
+            return new UserEntity()
+            {
+                UserId = Model.UserId,
+                BirthDate = Model.BirthDate,
+                Email = Model.Email,
+                NickName = Model.NickName,
+                Password = Model.Password,
+                Town = Model.Town,
+                Genre = Model.Genre
+            };
+        }
+
+
+        /// <summary>
+        /// Récupère une entité en base de donnée et l'affiche sous un autre modèle
+        /// </summary>
+        /// <param name="entite"></param>
+        /// <returns></returns>
+        public override UserModel? ToModel(UserEntity entite)
+        {
+            if (entite is null) return null;
+
+            return new UserModel()
+            {
+                UserId = entite.UserId,
+                BirthDate = entite.BirthDate,
+                Email = entite.Email,
+                NickName = entite.NickName,
+                Password = "***************",
+                Town = entite.Town,
+                Genre = entite.Genre
+            };
+        }
+    }
+}
---
+++ Butter_API/Controllers/UserController.cs
@@ -0,0 +1,89 @@
+﻿using Butter.Entities;
+using Butter.Models;
+using Butter.Repositories.Interfaces;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Butter_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        #region Injection de dépendance
+
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        } 
+
+        #endregion
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<UserModel> result = _userRepository.Get();
+
+            if (result.Count() == 0)
+                return NoContent();
+
+
+            return Ok(result);
+        }
+
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            UserModel result = _userRepository.GetById(id);
+
+            if (result is null)
+                return NoContent();
+
+
+            return Ok(result);
+        }
+
+
+        [HttpPost]
+        public IActionResult Add([FromBody] UserModel user)
+        {
+
+            _userRepository.Add(user);
+
+            return CreatedAtAction("Add", user);
+        }
+
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            if (!_userRepository.Delete(id))
+            {
+                return BadRequest();
+            };
+
+            return NoContent();
+        }
+
+
+        [HttpPatch("Update")]
+        public IActionResult Update([FromBody] UserModel user)
+        {
+       
+            if (user is not null)
+            {
+
+                _userRepository.Update(user);
+
+                return Ok(user);
+            }
+            return BadRequest();
+
+        }
+    }
+}
